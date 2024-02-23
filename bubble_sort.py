ee680f8fdd49a55012b0b1f0779586e1e423e66f
Caleb Bunch <100161536+CalebBunch@users.noreply.github.com>
2024-02-22 19:02:53 -0500

    clean up whitespaces in bubble_sort.py

--- bubble_sort.py
+++ bubble_sort.py
@@ -8,26 +8,17 @@ import tkinter as tk
 import time
 
 pygame.init()
-
 win = pygame.display.set_mode((1600, 900))
-
 pygame.display.set_caption("Bubble Sort")
 
 x = 40
-
 width = 10
-
 heights = [i * 10 for i in range(1, 78)]
-
 random.shuffle(heights)
 
-
 PURPLE = (255, 0, 255)
-
 AQUA = (0, 255, 255)
-
 BLACK = (0, 0, 0)
-
 
 sort_button = Button(
     win,
@@ -45,7 +36,6 @@ sort_button = Button(
     onRelease=lambda: sort(heights),
 )
 
-
 quit_button = Button(
     win,
     1470,
@@ -62,18 +52,12 @@ quit_button = Button(
     onRelease=lambda: force_quit(),
 )
 
-
 def speed_input():
     root = tk.Tk()
-
     root.title("Choose Speed")
-
     root.geometry("300x100")
-
     root.eval("tk::PlaceWindow . center")
-
     root.resizable(False, False)
-
     root.configure(background="black")
 
     def input_fast():
@@ -118,22 +102,15 @@ def speed_input():
 
     tk.mainloop()
 
-
 def counter(count=count(1)):
     return next(count)
 
-
 def display_heights(heights):
-
     for i in range(len(heights)):
-
         pygame.draw.rect(win, AQUA, (x + 20 * i, (900 - heights[i]), width, heights[i]))
 
-
 def change_color(heights, index1, index2):
-
     for i in range(len(heights)):
-
         if i == index1:
             pygame.draw.rect(
                 win,
@@ -152,101 +129,61 @@ def change_color(heights, index1, index2):
             )
             pygame.display.update()
 
-
 def bubble_sort_fast():
-
     for i in range(len(heights) - 1):
-
         swapped = False
-
         for j in range(len(heights) - i - 1):
-
             if heights[j] > heights[j + 1]:
-
                 heights[j], heights[j + 1] = heights[j + 1], heights[j]
-
                 swapped = True
-
             win.fill((0, 0, 0))
-
             display_heights(heights)
-
             change_color(heights, j, j + 1)
-
         if not swapped:
             break
-
 
 def bubble_sort_slow():
-
     for i in range(len(heights) - 1):
-
         swapped = False
-
         for j in range(len(heights) - i - 1):
-
             if heights[j] > heights[j + 1]:
-
                 heights[j], heights[j + 1] = heights[j + 1], heights[j]
-
                 swapped = True
-
             win.fill((0, 0, 0))
-
             display_heights(heights)
-
             change_color(heights, j, j + 1)
-
             pygame.time.delay(10)
-
         if not swapped:
             break
 
-
 def sort(heights):
-
     counter()
-
     if counter() - 1 == 1:
         speed_input()
-
     else:
         random.shuffle(heights)
         speed_input()
 
-
 def main():
     run = True
-
     clock = pygame.time.Clock()
-
     while run:
         clock.tick(60)
-
         keys = pygame.key.get_pressed()
-
         for event in pygame.event.get():
-
             if event.type == pygame.QUIT:
                 run = False
 
         if keys[pygame.K_q]:
             run = False
-
+        
         win.fill((0, 0, 0))
-
         display_heights(heights)
-
         pw.update(event)
-
         sort_button.listen(event)
-
         sort_button.draw()
-
         quit_button.listen(event)
-
         quit_button.draw()
-
         pygame.display.update()
 
     force_quit()
